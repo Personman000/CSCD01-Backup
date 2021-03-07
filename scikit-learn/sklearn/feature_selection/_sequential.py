b6ebfb7ca96b2140604c89e121a3b7ea1ae8b42e
adamahchong0 <60679239+adamahchong0@users.noreply.github.com>
2021-03-07 11:33:25 -0500

    Added documentation
    just added "default=None" for the y value that was changed to have default None

--- scikit-learn/sklearn/feature_selection/_sequential.py
+++ scikit-learn/sklearn/feature_selection/_sequential.py
@@ -122,7 +122,7 @@ class SequentialFeatureSelector(SelectorMixin, MetaEstimatorMixin,
         ----------
         X : array-like of shape (n_samples, n_features)
             Training vectors.
-        y : array-like of shape (n_samples,)
+        y : array-like of shape (n_samples,), default=None
             Target values.
 
         Returns
